f222ca6d663999a8207d1c8f28bbb03dd3f78e19
Lucas Bastos <lucsbasto@gmail.com>
2020-08-14 20:45:05 -0300

    add files and inputs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# find_number
-Programa que busca um número em um arquivo
+Considere que um arquivo de entrada (em formato texto) tenha o seguinte formato:
+a primeira linha contém o número ;
+a segunda linha possui um número inteiro , que corresponde à quantidade de números do conjunto ; e
+da terceira linha em diante, estão os números do conjunto .
+Crie um programa que leia o arquivo de entrada e procure o número  no conjunto , gerando um arquivo de saída contendo três números, um em cada linha:
+a palavra True, se , ou False, se ;
+um número inteiro , que corresponde à posição de  em . Se , então o valor de  deve ser ; e
+um número real , que corresponde ao tempo de execução do programa em milissegundos.
+Para testar o programa programa, use os três arquivos fornecidos como anexos na atividade.
+
---
+++ main.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+def find_number(numbers, output):
+    start_time = datetime.now().microsecond
+    number = numbers.pop(0)
+    size = numbers.pop(0)
+    if number not in numbers:
+        output.write('False\n')
+        output.write("%s\n" % -1)
+        output.write("%s" % (start_time - datetime.now().microsecond))
+    else:
+        output.write('True\n')
+        output.write("%s\n" % numbers.index(number))
+        output.write("%s\n" % (start_time - datetime.now().microsecond))
+for i in ['a', 'b', 'c']:
+    input_name = 'dataset-1-{i}.csv'.format(i=i)
+    output_name = 'resposta-dataset-1-{i}.txt'.format(i=i)
+    input = open(input_name, 'r')
+    output = open(output_name, 'w+');
+    lines = input.read();
+    numbers = lines.split("\n")
+    find_number(numbers, output)
