ac008dcc74266f9d8e5ea315b27c4ae071413714
Lucas Bastos <lucsbasto@gmail.com>
2020-08-14 21:06:22 -0300

    fix math

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def find_number(numbers, output):
     if number not in numbers:
         output.write('False\n')
         output.write("%s\n" % -1)
-        output.write("%s" % (start_time - datetime.now().microsecond))
+        output.write("%s" % (datetime.now().microsecond - start_time))
     else:
         output.write('True\n')
         output.write("%s\n" % numbers.index(number))
-        output.write("%s\n" % (start_time - datetime.now().microsecond))
+        output.write("%s\n" % (datetime.now().microsecond - start_time))
 for i in ['a', 'b', 'c']:
     input_name = 'dataset-1-{i}.csv'.format(i=i)
     output_name = 'resposta-dataset-1-{i}.txt'.format(i=i)
